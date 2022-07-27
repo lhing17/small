bfb75c5637630a844fde635d16ac9ba2da8d6f76
lhing17 <lhing17@163.com>
2022-07-27 15:54:35 +0800

    feat: 门派物品生成器。

---
+++ generator/src/generator/core.clj
@@ -0,0 +1,15 @@
+(ns generator.core
+  (:require [selmer.parser :as parser]
+            [clojure.java.io :as jio]))
+
+(defn generator-denom-item [denom]
+  (parser/render-file (jio/resource "denom-item.txt")
+    denom))
+
+
+(comment
+  (print (generator-denom-item {:id "I000"
+                         :name "拜入葵花派"
+                         :description "可习得武功：|n|CFF00FFCC惊涛掌  隔空打穴  龟壳神功|r|n|n门派毕业技|n|CFF00FFCC葵花心法|r|n|n门派附加属性|n|cFF00FF00根骨+3  医术+2|r"
+                         :art "ReplaceableTextures\\\\CommandButtons\\\\BTN50Icon.blp"}))
+  ,)
